b7b775447c496314ed2fd8ee9866411aa16271db
Marto-kun <martinvozme456@gmail.com>
2026-01-09 11:49:03 -0500

    Primer Commit, creacion de header para funciones de validacion

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main (int argc, char *argv[]) {
+
+    printf("\n----------------------------------------------------------------");
+    printf("\n          Sistema Integral de Gestion y \nPrediccion de Contaminacion del Aire en Zonas Urbanas");
+    printf("\n----------------------------------------------------------------\n");
+
+    return 0;
+}
