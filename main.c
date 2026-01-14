66fc132019df0ae256e2045e578e9e2c8b32fd99
Marto-kun <martinvozme456@gmail.com>
2026-01-14 18:32:37 -0500

    Agregar menú principal y validación de opciones en el sistema de gestión de contaminación del aire

--- main.c
+++ main.c
@@ -5,6 +5,8 @@ int main(int argc, char *argv[])
 {
 
     Zona zonas[NUM_ZONAS];
+    char entrada[50];
+    int opc, count;
     FILE *archivo = fopen("zonas.txt", "r");
 
     printf("\n----------------------------------------------------------------");
@@ -30,6 +32,65 @@ int main(int argc, char *argv[])
         fclose(archivo);
         printf("\nEl archivo fue creado con exito!");
     }
+
+    //---- MENU PRINCIPAL ----
+    do
+    {
+        int opcValida = 0;
+        do
+        {
+            printf("\n------ MENU PRINCIPAL ------");
+            printf("\n1) Ingresar nueva zona");
+            printf("\n2) Monitoreo de límites actuales");
+            printf("\n3) Predicciones de contaminacion (prox. 24 horas)");
+            printf("\n4) Reporte de promedios históricos (ultimos 30 dias)");
+            printf("\n5) Generar reporte final");
+            printf("\n6) Salir");
+            printf("\n>>>");
+
+            if (fgets(entrada, 25, stdin) == NULL)
+            {
+                LimpiarBuffer();
+                continue;
+            }
+
+            // Eliminar el salto de línea al final de la cadena
+            entrada[strcspn(entrada, "\n")] = '\0';
+
+            if (VerificacionDigitos(entrada))
+            {
+                opc = atoi(entrada);
+                opcValida = 1;
+            }
+            else
+            {
+                printf("\nSolo se permiten numeros enteros");
+            }
+
+        } while (opcValida == 0);
+
+
+        switch (opc)
+        {
+        case 1:
+            IngresarDatosZonas(zonas, count);
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        case 6:
+            break;
+        default:
+            printf("\nOpcion invalida, intentelo de nuevo...");
+            break;
+        }
+
+    } while (opc != 6);
 
     return 0;
 }
